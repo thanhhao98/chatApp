ffeef95f8274eaaf2b743796b3cd6e14eafb461a
oz <nghia.pq77@gmail.com>
2018-10-16 20:03:46 +0700

    DataClient finished

--- src/com/muc/DataClient.java
+++ src/com/muc/DataClient.java
@@ -2,46 +2,154 @@ package com.muc;
 
 import java.util.ArrayList;
 
+import java.io.*;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import org.w3c.dom.*;
+
 public class DataClient {
     // setup path of datafile
-    private final static String pathDataFile = "";
+    private final static String pathDataFile = "Data.xml";
     private String username=null;
     private String password=null;
-    private  String id=null;
-    private  boolean isLogin = false;
+    private String id=null;
 
-    public DataClient(String username, String password){
-        this.username = username;
-        this.password = password;
-    }
+//    public DataClient(String username, String password){
+//        this.username = username;
+//        this.password = password;
+//    }
 
-    public DataClient(String id, String username, String email, String name, String password) {
+    public DataClient(String id, String username, String password) {
         this.id = id;
         this.username = username;
         this.password = password;
-        this.isLogin = true;
     }
 
     private ArrayList<DataClient> getDataFromDataBase(){
-        return null;
+        ArrayList<DataClient> ret_val = new ArrayList();
+        try{
+            File fXmlFile = new File(pathDataFile);
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(fXmlFile);
+            doc.getDocumentElement().normalize();
+
+            NodeList nList = doc.getElementsByTagName("user");
+
+            for (int temp = 0; temp < nList.getLength(); temp++) {
+                Node nNode = nList.item(temp);
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) nNode;
+                    String id = getTagValue("id", eElement);
+                    String username = getTagValue("username", eElement);
+                    String pass = getTagValue("password", eElement);
+                    DataClient arr_ele = new DataClient(id, username, pass);
+                    ret_val.add(arr_ele);
+                }
+            }
+        }
+        catch(Exception ex){
+            System.out.println(ex);
+        }
+        return ret_val;
     }
 
-    public boolean checkLogin(DataClient client){
-        return true;
+    public boolean checkUsernameExist(DataClient client){
+        try{
+            File fXmlFile = new File(pathDataFile);
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(fXmlFile);
+            doc.getDocumentElement().normalize();
+
+            NodeList nList = doc.getElementsByTagName("user");
+            for (int temp = 0; temp < nList.getLength(); temp++) {
+                Node nNode = nList.item(temp);
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) nNode;
+                    String username = getTagValue("username", eElement);
+                    if (client.getUsername().equals(username)) return true;
+                }
+            }
+        }
+        catch(Exception ex){
+            System.out.println(ex);
+        }
+        return false;
     }
 
-    // Check info of curren client
+    // Check info of current client
     public boolean checkInfo(){
+
         return true;
     }
 
     // Check -> add data of client to Data
     public boolean addDataClient(){
+        try {
+            DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+            Document doc = docBuilder.parse(pathDataFile);
+
+            Node data = doc.getFirstChild();
+
+            Element newuser = doc.createElement("user");
+            Element newid = doc.createElement("id"); newid.setTextContent(this.id);
+            Element newusername = doc.createElement("username"); newusername.setTextContent(this.username);
+            Element newpassword = doc.createElement("password"); newpassword.setTextContent(this.password);
+
+            newuser.appendChild(newid); newuser.appendChild(newusername); newuser.appendChild(newpassword);
+            data.appendChild(newuser);
+
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource source = new DOMSource(doc);
+            StreamResult result = new StreamResult(new File(pathDataFile));
+            transformer.transform(source, result);
+        }
+        catch(Exception ex){
+            System.out.println(ex);
+            return false;
+        }
         return true;
     }
 
     public boolean removeDataClient(){
-        return true ;
+        boolean val = false;
+        try{
+            File fXmlFile = new File(pathDataFile);
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(fXmlFile);
+            doc.getDocumentElement().normalize();
+
+            NodeList nList = doc.getElementsByTagName("user");
+
+            for (int temp = 0; temp < nList.getLength(); temp++) {
+                Node nNode = nList.item(temp);
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) nNode;
+                    String username = getTagValue("username", eElement);
+                    if (this.getUsername().equals(username)){
+                        eElement.getParentNode().removeChild(nNode);
+                        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+                        Transformer transformer = transformerFactory.newTransformer();
+                        DOMSource source = new DOMSource(doc);
+                        StreamResult result = new StreamResult(new File(pathDataFile));
+                        transformer.transform(source, result);
+                        val = true;
+                    }
+                }
+            }
+        }
+        catch(Exception ex){
+            System.out.println(ex);
+        }
+        return val;
     }
 
     public String getUsername(){
@@ -56,5 +164,9 @@ public class DataClient {
         return this.id;
     }
 
-
+    public static String getTagValue(String sTag, Element eElement) {
+        NodeList nlList = eElement.getElementsByTagName(sTag).item(0).getChildNodes();
+        Node nValue = (Node) nlList.item(0);
+        return nValue.getNodeValue();
+    }
 }
